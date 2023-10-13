dc80cfe27e3612da58572cf211f9c2ba5e0a828c
Archismwan Chatterjee <115975340+ArchismwanChatterjee@users.noreply.github.com>
2023-10-14 02:51:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+  (function (w, d, s, o, f, js, fjs) {
+    w["botsonic_widget"] = o;
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+    js.id = o;
+    js.src = f;
+    js.async = 1;
+    fjs.parentNode.insertBefore(js, fjs);
+  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+  Botsonic("init", {
+    serviceBaseUrl: "https://api.botsonic.ai",
+    token: "fefbbfd7-2654-4da6-9f5d-05a8f1a27d7f",
+  });
+</script>
